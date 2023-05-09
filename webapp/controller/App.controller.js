43f720fdc437b083962f94cf93f2a0d0e4bf8b8d
TodoniK <jules.royet.pc@gmail.com>
2023-05-09 10:08:54 +0200

    BI separated from the controller and adding some error control messages

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,43 +7,9 @@ sap.ui.define([
 	return Controller.extend("sap.ui.demo.todo.controller.App", {
 		
 		onInit: function() {
-			var table = new TableComponent();
-			var oModel = new sap.ui.model.json.JSONModel();
-						
-			oModel.setData(table.getDataFromApi("users"));
-
-			// Recuperer la table de la vue
-			var oTable = this.getView().byId("table");
-			console.log(oTable);
-			
-			// On suppose que les données sont stockées dans le modèle oModel
-			var aData = oModel.getData();
-			console.log(aData);
-
-			if (aData.length > 0) {
-				// On récupère les propriétés de l'objet JSON de l'élément 0
-				var aKeys = Object.keys(aData[0]);
-				console.log(aKeys);
-
-				// On crée une colonne pour chaque propriété
-				aKeys.forEach(function(sKey) {
-					console.log(sKey);
-					oTable.addColumn(new sap.m.Column({
-						header: new sap.m.Label({ text: sKey })
-					}));
-				});
-
-				oTable.setModel(oModel);
-				oTable.bindItems("/", new sap.m.ColumnListItem({
-					cells: aKeys.map(function(sKey) {
-						return new sap.m.Text({
-							text: "{" + sKey + "}"
-						});
-					}
-					)
-				}));
-			}
-		}
-		
+			var viewedTable = this.getView().byId("table");
+			var tableComponent = new TableComponent();
+			tableComponent.fillTable(viewedTable);
+		}	
 	});
 });
--- webapp/resources/TableComponent/TableComponent.js
+++ webapp/resources/TableComponent/TableComponent.js
@@ -1,9 +1,6 @@
 sap.ui.define([
   "sap/ui/core/UIComponent",
-  "sap/ui/core/ComponentSupport",
-  "sap/ui/model/json/JSONModel",
-  "jquery.sap.global",
-], function(UIComponent, JSONModel, jQuery) {
+], function(UIComponent) {
   "use strict";
 
   return UIComponent.extend("openui5-sample-app.TableComponent", {
@@ -25,25 +22,59 @@ sap.ui.define([
       return this.getProperty("tableName");
     },
 
+    fillTable: function(viewedTable) {
+    
+      var oModel = new sap.ui.model.json.JSONModel();
+						
+			oModel.setData(this.getDataFromApi("users"));
+
+      if(oModel.getData() != undefined) {
+
+        // On suppose que les données sont stockées dans le modèle oModel
+        var aData = oModel.getData();
+
+        if (aData.length > 0) {
+          // On récupère les propriétés de l'objet JSON de l'élément 0
+          var aKeys = Object.keys(aData[0]);
+
+          // On crée une colonne pour chaque propriété
+          aKeys.forEach(function(sKey) {
+            viewedTable.addColumn(new sap.m.Column({
+              header: new sap.m.Label({ text: sKey })
+            }));
+          });
+
+          viewedTable.setModel(oModel);
+          viewedTable.bindItems("/", new sap.m.ColumnListItem({
+            cells: aKeys.map(function(sKey) {
+              return new sap.m.Text({
+                text: "{" + sKey + "}"
+              });
+            })
+          }));
+        }
+
+      } else {
+        Swal.fire({icon: 'error', title: 'Oops...', text: 'No db connection for this table!'});
+      }
+    },
+
     getDataFromApi: function(sTableName) {
       this.setTableName(sTableName);
 
-      if(this.getProperty("tableName") == "") {
-          console.log("You must enter a table name");
-      } else {
-          var sUrl = "http://localhost:3000/api/" + this.getTableName();
-          var result = false;
-    
-          result = $.ajax({
-            url: sUrl,
-            async: false,
-            dataType: 'json',
-            success: function(sResult){}})
-          .responseJSON;
+      var sUrl = "http://localhost:3000/api/" + this.getTableName();
+      var result = false;
 
-          return result;
-      }
+      result = $.ajax({
+        url: sUrl,
+        async: false,
+        dataType: 'json',
+        success: function(sResult){},
+        error: function(sResult){},
+      })
+      .responseJSON;
+
+      return result;
     }
-    
   });
 });
