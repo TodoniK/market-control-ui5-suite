91ddc9b27cbbde39026599d48cc02b23cdae4a9d
TodoniK <jules.royet.pc@gmail.com>
2023-05-09 09:31:34 +0200

    Table reusable openui5 component fully working dynamically, going to separate now the BI from the controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,9 +10,39 @@ sap.ui.define([
 			var table = new TableComponent();
 			var oModel = new sap.ui.model.json.JSONModel();
 						
-			oModel.setData(table.getDataFromApi("orders"));
+			oModel.setData(table.getDataFromApi("users"));
 
-			this.getView().setModel(oModel);
+			// Recuperer la table de la vue
+			var oTable = this.getView().byId("table");
+			console.log(oTable);
+			
+			// On suppose que les données sont stockées dans le modèle oModel
+			var aData = oModel.getData();
+			console.log(aData);
+
+			if (aData.length > 0) {
+				// On récupère les propriétés de l'objet JSON de l'élément 0
+				var aKeys = Object.keys(aData[0]);
+				console.log(aKeys);
+
+				// On crée une colonne pour chaque propriété
+				aKeys.forEach(function(sKey) {
+					console.log(sKey);
+					oTable.addColumn(new sap.m.Column({
+						header: new sap.m.Label({ text: sKey })
+					}));
+				});
+
+				oTable.setModel(oModel);
+				oTable.bindItems("/", new sap.m.ColumnListItem({
+					cells: aKeys.map(function(sKey) {
+						return new sap.m.Text({
+							text: "{" + sKey + "}"
+						});
+					}
+					)
+				}));
+			}
 		}
 		
 	});
